284d311a6b55bf72787595914ad5be988dc4a716
dexstar <dexstaar@gmail.com>
2017-12-31 10:39:53 +0800

    Done sec 5

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,47 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+
+
+
+// Solution 2
+function palindrome(str) {
+    return str.split('').every((char, i) => {
+        return char === str[str.length - i - 1];
+    });
+}
+
+
+
+
+// Solution 1
+// function palindrome(str) {
+//     const reversed = str.split('').reverse().join('');
+//
+//     return str === reversed;
+// }
+
+
+
+// My solution
+// function palindrome(str) {
+//     const arr = str.split('');
+//     const stack = [];
+//     let truningPoint = 0;
+//
+//     (arr.length % 2 === 0) ? truningPoint = arr.length/2 : truningPoint = arr.length/2-1;
+//
+//     for(let i=0; i<arr.length; i++) {
+//         if(i<truningPoint) {
+//             stack.push(arr[i]);
+//         } else {
+//             if (stack[stack.length-1] === arr[i]) stack.pop();
+//         }
+//     }
+//
+//     if(stack.length === 0) return true;
+//
+//     return false;
+// }
 
 module.exports = palindrome;
