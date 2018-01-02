01111450d2ecfa6982b352ac52ffa4b8a3ad0f7a
Kim, JeongHoon <jeonghoon.kim@rakuten.com>
2018-01-02 20:46:03 +0800

    not solved yet

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,47 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  // console.log("array :", array);
+
+  let rtnArr = [];
+  let cnt = 0;
+  let tempArr = [];
+
+  array.forEach((elem) => {
+
+    tempArr.push(elem);
+    cnt++;
+
+    if(cnt === size) {
+      rtnArr.push(tempArr);
+      cnt = 0;
+      tempArr = [];
+
+    }
+    
+
+
+
+    // if (cnt < size) {
+    //   tempArr.push(elem);
+    //   cnt++;
+
+    //   if(i === array.length-1) rtnArr.push(tempArr);
+
+    // } else {
+    //   // console.log("tempArr :", tempArr);
+    //   // console.log("elem :", elem);
+    //   rtnArr.push(tempArr);
+    //   cnt = 0;
+    //   tempArr = [];
+    //   tempArr.push(elem);
+    // }
+
+
+  });
+
+  return rtnArr;
+}
 
 module.exports = chunk;
