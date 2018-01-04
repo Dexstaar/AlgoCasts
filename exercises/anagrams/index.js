9bccbeb3fe3a55db778d6f33760bb2f325a289f1
dexstar <dexstaar@gmail.com>
2018-01-04 20:47:13 +0800

    Done lec 30

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,80 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+
+// solution 1
+function anagrams(stringA, stringB) {
+    const aCharMap = buildCharMap(stringA);
+    const bCharMap = buildCharMap(stringB);
+
+    console.log('aCharMap : ', aCharMap);
+    console.log('bCharMap : ', bCharMap);
+
+    if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+        return false;
+    }
+
+    for (let char in aCharMap) {
+        if (aCharMap[char] !== bCharMap[char]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function buildCharMap(str) {
+    const charMap = {};
+
+    for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+
+    return charMap;
+}
+
+
+
+
+
+// My solution
+// function anagrams(stringA, stringB) {
+//     const store = [];
+//
+//     let rtnVal = true;
+//
+//     stringA.split('').forEach(elem => {
+//         if(elem != ' ') {
+//             if(store[elem]) {
+//                 store[elem]++;
+//             } else {
+//                 store[elem] = 1;
+//             }
+//         }
+//     });
+//
+//     stringB.split('').forEach(elem => {
+//         if(elem != ' ') {
+//             console.log('store[elem] : ', store[elem]);
+//             if(!store[elem]) {
+//                 rtnVal = false;
+//             }
+//
+//             if(store[elem] > 0) {
+//                 store[elem]--;
+//             } else {
+//                 rtnVal = false;
+//             }
+//         }
+//     });
+//
+//     stringA.split('').forEach(elem => {
+//         if(store[elem]) {
+//             rtnVal = false;
+//         }
+//     });
+//
+//     return rtnVal;
+// }
 
 module.exports = anagrams;
