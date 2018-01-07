f4ad4eead6f7eb2078db12c38285a1a62b0fa31c
dexstar <dexstaar@gmail.com>
2018-01-07 12:10:25 +0800

    Done sec 11

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,88 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+
+
+
+function steps(n, row = 0, stair = '') {
+    if (n === row) return;
+
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row+1);
+    }
+
+    const add = (stair.length <= row) ? '#' : ' ';
+
+    steps(n, row, stair+add);
+}
+
+
+
+
+
+// solution 2
+// function steps(n, row = 0, stair = '') {
+//     if (n === row) {
+//         return;
+//     }
+//
+//     if (n === stair.length) {
+//         console.log(stair);
+//         return steps(n, row + 1);
+//     }
+//
+//     const add = stair.length <= row ? '#' : ' ';
+//
+//     // if (stair.length <= row) {
+//     //     stair += '#';
+//     // } else {
+//     //     stair += ' ';
+//     // }
+//
+//     steps(n, row, stair + add);
+// }
+
+
+
+
+// solution 1
+// function steps(n) {
+//     for (let row = 0; row < n; row++) {
+//         let stair = '';
+//
+//         for (let column = 0; column < n; column++) {
+//             if (column <= row) {
+//                 stair += '#';
+//             } else {
+//                 stair += ' ';
+//             }
+//         }
+//
+//         console.log(stair);
+//     }
+// }
+
+
+
+// my solution
+// function steps(n) {
+//     let cnt = 0;
+//     let str = '';
+//
+//     for(let i=0; i<n; i++){
+//         for(let j=0; j<n; j++) {
+//             if(j<=cnt) {
+//                 str += '#';
+//             } else {
+//                 str += ' ';
+//             }
+//         }
+//
+//         console.log(str);
+//         cnt++;
+//         str = '';
+//     }
+// }
 
 module.exports = steps;
