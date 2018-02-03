5f8ed4555b4db237fa75fa1538430cc05fa9b15c
dexstar <dexstaar@gmail.com>
2018-02-03 15:27:02 +0800

    done sec 12,13

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,36 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// solution 2
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
+
+// solution 1
+// function vowels(str) {
+//   let count = 0;
+//   const checker = ["a", "e", "i", "o", "u"];
+
+//   for (let char of str.toLowerCase()) {
+//     if (checker.includes(char)) {
+//       count++;
+//     }
+//   }
+
+//   return count;
+// }
+
+// my solution
+// function vowels(str) {
+//     let cnt = 0;
+//     vowels = ['a', 'e', 'i', 'o', 'u'];
+
+//     str.split('').forEach(elem => {
+//         if(vowels.indexOf(elem.toLowerCase()) > -1) cnt++;
+//     });
+
+//     return cnt;
+// }
 
 module.exports = vowels;
