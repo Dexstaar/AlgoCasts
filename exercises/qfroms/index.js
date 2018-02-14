3312df55337dc1c5b49dfed69e753cfbb5489e33
dexstar <dexstaar@gmail.com>
2018-02-14 18:35:40 +0800

    Done sec 20

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,73 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+// solution 1
+class Queue {
+    constructor() {
+        this.first = new Stack();
+        this.second = new Stack();
+    }
+
+    add(record) {
+        this.first.push(record);
+    }
+
+    remove() {
+        while (this.first.peek()) {
+            this.second.push(this.first.pop());
+        }
+
+        const record = this.second.pop();
+
+        while (this.second.peek()) {
+            this.first.push(this.second.pop());
+        }
+
+        return record;
+    }
+
+    peek() {
+        while (this.first.peek()) {
+            this.second.push(this.first.pop());
+        }
+
+        const record = this.second.peek();
+
+        while (this.second.peek()) {
+            this.first.push(this.second.pop());
+        }
+
+        return record;
+    }
+}
+
+
+// My solution
+// class Queue {
+//     constructor() {
+//         this.main = new Stack();
+//         this.sub = new Stack();
+//     }
+
+//     add(element) {
+//         while(this.main.peek()) {
+//             this.sub.push(this.main.pop())
+//         }
+
+//         this.main.push(element);
+
+//         while(this.sub.peek()) {
+//             this.main.push(this.sub.pop());
+//         }
+//     }
+
+//     remove() {
+//         return this.main.pop();
+//     }
+
+//     peek() {
+//         return this.main.peek();
+//     }
+// }
 
 module.exports = Queue;
--- exercises/weave/index.js
+++ exercises/weave/index.js
@@ -24,6 +24,36 @@
 
 const Queue = require('./queue');
 
-function weave(sourceOne, sourceTwo) {}
+
+// my solution
+function weave(sourceOne, sourceTwo) {
+    const q = new Queue();
+
+    while(sourceOne.peek() || sourceTwo.peek()) {
+        if(sourceOne.peek()) {
+            q.add(sourceOne.remove());
+        }
+
+        if(sourceTwo.peek()) {
+            q.add(sourceTwo.remove());
+        }
+    }
+
+    return q;
+}
+
+
+
+// my solution
+// function weave(sourceOne, sourceTwo) {
+//     const weavedQueue = new Queue();
+
+//     while(sourceOne.peek() || sourceTwo.peek()) {
+//         if(sourceOne.peek()) weavedQueue.add(sourceOne.remove());
+//         if(sourceTwo.peek()) weavedQueue.add(sourceTwo.remove());
+//     }
+
+//     return weavedQueue;
+// }
 
 module.exports = weave;
