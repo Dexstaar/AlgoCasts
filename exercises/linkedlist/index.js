00d9034c581e20fdbc6d9471b87f9976dd463401
dexstar <dexstaar@gmail.com>
2018-02-17 17:28:27 +0800

    Done sec 21.

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -15,7 +15,8 @@ class LinkedList {
   }
 
   insertFirst(data) {
-    this.head = new Node(data, this.head);
+    // this.head = new Node(data, this.head);
+    this.insertAt(data, 0);
   }
 
   size() {
@@ -35,14 +36,15 @@ class LinkedList {
   }
 
   getLast() {
-    if (!this.head) return null;
+    // if (!this.head) return null;
 
-    let node = this.head;
+    // let node = this.head;
 
-    while (node) {
-      if(!node.next) return node;
-      node = node.next;
-    }
+    // while (node) {
+    //   if(!node.next) return node;
+    //   node = node.next;
+    // }
+    return this.getAt(this.size()-1);
   }
 
   clear() {
@@ -125,7 +127,26 @@ class LinkedList {
 
     const previous = this.getAt(index-1) || this.getLast();
     const node = new Node(data, previous.next);
+
     previous.next = node;
+  }
+
+  forEach(fn) {
+    let node = this.head;
+    let counter = 0;
+    while (node) {
+      fn(node, counter);
+      node = node.next;
+      counter++;
+    }
+  }
+
+  *[Symbol.iterator]() {
+    let node = this.head;
+    while (node) {
+      yield node;
+      node = node.next;
+    }
   }
 
 
