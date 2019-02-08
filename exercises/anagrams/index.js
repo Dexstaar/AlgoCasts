4da38a84e4e366c0c459aa818d444e015c1f96b9
dexstar <dexstaar@gmail.com>
2019-02-08 19:42:49 +0800

    .

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -12,32 +12,7 @@
 // str.replace(/[^\w]/g, '')
 
 function anagrams(stringA, stringB) {
-  const aCharMap = buildCharMap(stringA);
-  const bCharMap = buildCharMap(stringB);
-
-  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-    return false;
-  }
-
-  for (let char in aCharMap) {
-    if (aCharMap[char] !== bCharMap[char]) {
-      return false;
-    }
-  }
-
-  return true;
 }
-
-function buildCharMap(str) {
-  const charMap = {};
-
-  for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
-    charMap[char] = charMap[char] + 1 || 1;
-  }
-
-  return charMap;
-}
-
 
 
 
